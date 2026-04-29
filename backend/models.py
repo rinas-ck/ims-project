4a88ead37e1b9a485609683b31e30f74d81b4f7f
Mohammed Rinas <147690037+rinas-ck@users.noreply.github.com>
2026-04-29 18:44:19 +0530

    Add Incident model with relevant fields

--- backend/models.py
+++ backend/models.py
@@ -1,1 +1,10 @@
+from sqlalchemy import Column, Integer, String
+from db import Base
 
+class Incident(Base):
+    __tablename__ = "incidents"
+
+    id = Column(Integer, primary_key=True, index=True)
+    component = Column(String)
+    status = Column(String, default="OPEN")
+    rca = Column(String, nullable=True)
